99655995b4a8cfcfa26323eba155970e10de859a
Cj-Rodriguez101 <chibuikeoffu@gmail.com>
2020-09-11 22:59:14 +0100

    set the link finally

--- app/src/main/java/com/cjproductions/gadsproject1/api/SubmitApiService.kt
+++ app/src/main/java/com/cjproductions/gadsproject1/api/SubmitApiService.kt
@@ -3,6 +3,14 @@ package com.cjproductions.gadsproject1.api
 import androidx.lifecycle.LiveData
 import com.cjproductions.gadsproject1.api.response.SubmitResponse
 import com.cjproductions.gadsproject1.models.SkilledIndividual
+import com.cjproductions.gadsproject1.util.Constants.Companion.EMAIL_FINAL
+import com.cjproductions.gadsproject1.util.Constants.Companion.EMAIL_TEST
+import com.cjproductions.gadsproject1.util.Constants.Companion.LAST_NAME_FINAL
+import com.cjproductions.gadsproject1.util.Constants.Companion.LAST_NAME_TEST
+import com.cjproductions.gadsproject1.util.Constants.Companion.LINK_FINAL
+import com.cjproductions.gadsproject1.util.Constants.Companion.LINK_TEST
+import com.cjproductions.gadsproject1.util.Constants.Companion.NAME_FINAL
+import com.cjproductions.gadsproject1.util.Constants.Companion.NAME_TEST
 import com.cjproductions.gadsproject1.util.GenericApiResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -14,9 +22,9 @@ interface SubmitApiService {
     @POST("formResponse")
     @FormUrlEncoded
     fun sendSubmitResponse(
-        @Field("entry.1824927963") email: String,
-        @Field("entry.1877115667") name: String,
-        @Field("entry.2006916086") lastName: String,
-        @Field("entry.284483984") linkToProject: String
+        @Field(EMAIL_FINAL) email: String,
+        @Field(NAME_FINAL) name: String,
+        @Field(LAST_NAME_FINAL) lastName: String,
+        @Field(LINK_FINAL) linkToProject: String
     ): LiveData<GenericApiResponse<Any>>
 }
--- app/src/main/java/com/cjproductions/gadsproject1/util/Constants.kt
+++ app/src/main/java/com/cjproductions/gadsproject1/util/Constants.kt
@@ -6,6 +6,14 @@ class Constants {
         const val BASE_URL = " https://gadsapi.herokuapp.com/"
         const val FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/"
         const val FORM_URL_TESTING = "https://docs.google.com/forms/d/e/1FAIpQLSfNyKEoZoumc0gz-AtsorpX0ngK-cS-CLa62_FdTJ-oR2S1qQ/"
+        const val EMAIL_TEST = "entry.1063357417"
+        const val EMAIL_FINAL = "entry.1824927963"
+        const val NAME_TEST = "entry.835378002"
+        const val NAME_FINAL = "entry.1877115667"
+        const val LAST_NAME_TEST = "entry.167141549"
+        const val LAST_NAME_FINAL = "entry.2006916086"
+        const val LINK_TEST = "entry.1419069103"
+        const val LINK_FINAL = "entry.284483984"
         const val NETWORK_TIMEOUT = 4000L
         const val ERROR_FOR_SUBMISSION = "error for submission"
         const val TESTING_NETWORK_DELAY = 0L // fake network delay for testing
